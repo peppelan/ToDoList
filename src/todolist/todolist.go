2893ab96a0589869916f679adbdf145f85fc84b1
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-09-23 10:31:32 +0200

    Add basic 404 unit test

--- src/todolist/todolist.go
+++ src/todolist/todolist.go
@@ -10,10 +10,13 @@ import (
 )
 
 func main() {
+    log.Fatal(http.ListenAndServe(":8080", Handler()))
+}
 
+func Handler() http.Handler {
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
-    log.Fatal(http.ListenAndServe(":8080", router))
+    return router
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
---
+++ src/todolist/todolist_test.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"strings"
+	"testing"
+	"net/http/httptest"
+	"io/ioutil"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNonExistingAddress(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	w := httptest.NewRecorder()
+	Handler().ServeHTTP(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	respString := string(body)
+
+	require.Equal(t, 404, resp.StatusCode)
+	require.Contains(t, strings.ToLower(respString), "not found")
+}
