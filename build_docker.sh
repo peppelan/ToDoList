15d84acb0c16b162d62c7e833920f66f47c713fb
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-09-23 11:00:25 +0200

    Improve build command line

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 mkdir -p bin
-CGO_ENABLED=0 GOOS=linux go build -o bin/todolist src/todolist/todolist.go
+CGO_ENABLED=0 GOOS=linux go build -o bin/todolist todolist
 docker build -t to-do-list .
 
