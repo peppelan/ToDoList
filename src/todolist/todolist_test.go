fdb6febbf3ff5a7d499aa350457e27555f84328d
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-09-23 11:02:55 +0200

    Extend UTs

--- src/todolist/todolist_test.go
+++ src/todolist/todolist_test.go
@@ -9,7 +9,7 @@ import(
 	"github.com/stretchr/testify/require"
 )
 
-func TestNonExistingAddress(t *testing.T) {
+func TestNonHandledEndpoint(t *testing.T) {
 	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
 	w := httptest.NewRecorder()
 	Handler().ServeHTTP(w, req)
@@ -20,4 +20,17 @@ func TestNonExistingAddress(t *testing.T) {
 
 	require.Equal(t, 404, resp.StatusCode)
 	require.Contains(t, strings.ToLower(respString), "not found")
+}
+
+func TestRoot(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://example.com/", nil)
+	w := httptest.NewRecorder()
+	Handler().ServeHTTP(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	respString := string(body)
+
+	require.Equal(t, 200, resp.StatusCode)
+	require.Equal(t, respString, "Hello, \"/\"")
 }
