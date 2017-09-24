0620c4308eb2a10174d6e261b273927ad5b5e63c
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-09-24 20:42:48 +0200

    Add unit test for the /todos endpoint

--- src/todolist/todolist_test.go
+++ src/todolist/todolist_test.go
@@ -6,7 +6,9 @@ import (
 	"strings"
 	"testing"
 
+	"encoding/json"
 	"github.com/stretchr/testify/require"
+	"todolist/repo"
 )
 
 func TestNonHandledEndpoint(t *testing.T) {
@@ -33,4 +35,21 @@ func TestRoot(t *testing.T) {
 
 	require.Equal(t, 200, resp.StatusCode)
 	require.Equal(t, "Welcome!\n", respString)
+}
+
+func TestTodoIndex(t *testing.T) {
+	repository = repo.NewInMemoryRepo()
+	bytes, _ := json.Marshal(repository.FindAll())
+	expected := string(bytes) + "\n"
+
+	req := httptest.NewRequest("GET", "http://example.com/todos", nil)
+	w := httptest.NewRecorder()
+	NewRouter().ServeHTTP(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	respString := string(body)
+
+	require.Equal(t, 200, resp.StatusCode)
+	require.Equal(t, expected, respString)
 }
