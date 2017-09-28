f95c0dce077b300c39588909b61be2811be7feff
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-09-28 21:53:38 +0200

    Change return type of repo.FindAll

--- src/todolist/repo/inmemory.go
+++ src/todolist/repo/inmemory.go
@@ -7,7 +7,7 @@ import (
 
 type InMemoryRepo struct {
 	currentId int
-	todos map[string] *spi.Todo
+	todos map[string] spi.Todo
 }
 
 func NewInMemoryRepo() *InMemoryRepo {
@@ -16,15 +16,22 @@ func NewInMemoryRepo() *InMemoryRepo {
 }
 
 func (r *InMemoryRepo) Init() error {
-	r.todos = make(map[string] *spi.Todo)
+	r.todos = make(map[string] spi.Todo)
 	return nil
 }
 
 func (r *InMemoryRepo) Find(id string) *spi.Todo {
-	return r.todos[id]
+	t := r.todos[id]
+
+	if (spi.Todo{}) != t {
+		return &t
+	} else {
+		return nil
+	}
+
 }
 
-func (r *InMemoryRepo) FindAll() map[string] *spi.Todo {
+func (r *InMemoryRepo) FindAll() map[string] spi.Todo {
 	// FIXME: do not use for production code :-)
 	return r.todos
 }
@@ -32,20 +39,20 @@ func (r *InMemoryRepo) FindAll() map[string] *spi.Todo {
 func (r *InMemoryRepo) Create(t spi.Todo) string {
 	r.currentId += 1
 	id := fmt.Sprintf("%d", r.currentId)
-	r.todos[id] = &t
+	r.todos[id] = t
 	return id
 }
 
 func (r *InMemoryRepo) Destroy(id string) bool {
-	isPresent := nil != r.todos[id]
+	isPresent := spi.Todo{} != r.todos[id]
 	delete(r.todos, id)
 	return isPresent
 }
 
 func (r *InMemoryRepo) Update(id string, todo spi.Todo) bool {
-	isPresent := nil != r.todos[id]
+	isPresent := spi.Todo{} != r.todos[id]
 	if isPresent {
-		r.todos[id] = &todo
+		r.todos[id] = todo
 		return true
 	} else {
 		return false
