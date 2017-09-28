c3640ccf717a863834fd552d5a18c1e55bcb8594
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-09-28 10:10:11 +0200

    Rely on the ELASTICSEARCH_URL env variable
    the choice of its name is consistent with other applications by Elasticsearch such as Kibana
    see also: https://www.elastic.co/guide/en/kibana/current/_configuring_kibana_on_docker.html
    
    Relates to: #6

--- src/todolist/repo/factory.go
+++ src/todolist/repo/factory.go
@@ -1,10 +1,26 @@
 package repo
 
-import "todolist/spi"
+import (
+	"todolist/spi"
+	"os"
+	"errors"
+)
 
-// Creates the appropriate repository depending on the application configuration
+var (
+	esUrl = os.Getenv("ELASTICSEARCH_URL")
+	esLogin = os.Getenv("ELASTICSEARCH_USERNAME")
+	esPwd = os.Getenv("ELASTICSEARCH_PASSWORD")
+)
+
+// Creates the appropriate repository depending on the application configuration:
+// - set the environment variable ELASTICSEARCH_URL to back the application with ElasticSearch
+// - leave the variable unset to let the application use an in-memory repository
 func NewRepo() spi.Repo {
-	r := NewInMemoryRepo()
-	r.Init()
-	return r
+	if "" == esUrl {
+		r := NewInMemoryRepo()
+		r.Init()
+		return r
+	}
+
+	panic(errors.New("elasticsearch repo not implemented yet"))
 }
