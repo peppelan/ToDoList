8d2861476b7757f5e2e89ecd3609aadf0c79d3eb
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2017-10-05 20:22:51 +0200

    Add zipkin tracing

--- src/todolist/router.go
+++ src/todolist/router.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	opentracing "github.com/opentracing/opentracing-go"
+	zipkin "github.com/openzipkin/zipkin-go-opentracing"
 	"log"
 	"net/http"
+	"os"
 	"time"
+)
+
+var (
+	zipkinUrl = os.Getenv("ZIPKIN_URL")
 )
 
 // Creates a router for the ToDoList app.
@@ -20,6 +28,35 @@ func NewRouter() *mux.Router {
 			Path(route.Pattern).
 			Name(route.Name).
 			Handler(logger(route.HandlerFunc, route.Name))
+	}
+
+	if "" != zipkinUrl {
+		// Create our HTTP collector.
+		collector, err := zipkin.NewHTTPCollector(zipkinUrl + "/api/v1/spans")
+		if err != nil {
+			fmt.Printf("unable to create Zipkin HTTP collector: %+v\n", err)
+			os.Exit(-1)
+		}
+
+		// Create our recorder.
+		recorder := zipkin.NewRecorder(collector, false, "0.0.0.0:0", "todolist")
+
+		// Create our tracer.
+		tracer, err := zipkin.NewTracer(
+			recorder,
+			zipkin.ClientServerSameSpan(true),
+			zipkin.TraceID128Bit(true),
+		)
+		if err != nil {
+			fmt.Printf("unable to create Zipkin tracer: %+v\n", err)
+			os.Exit(-1)
+		}
+
+		// Explicitly set our tracer to be the default tracer.
+		opentracing.InitGlobalTracer(tracer)
+
+		fmt.Printf("Initialized tracer")
+
 	}
 
 	return router
@@ -47,7 +84,19 @@ func logger(inner http.Handler, name string) http.Handler {
 				)
 			}
 		}()
-		inner.ServeHTTP(responseWriter, r)
+
+		if "" != zipkinUrl {
+			// Define a trace
+			opentracing.StartSpan(name)
+			span, ctx := opentracing.StartSpanFromContext(r.Context(), name)
+			defer span.Finish()
+
+			// Serve the request, and propagate the new context
+			inner.ServeHTTP(responseWriter, r.WithContext(ctx))
+		} else {
+			// Serve the request only
+			inner.ServeHTTP(responseWriter, r)
+		}
 
 		log.Printf(
 			"%s\t%s\t%s\t%s\t%d (%s)\t%d us",
